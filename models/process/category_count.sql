66f2d06bb259b51f6e7cccb9bfe3d57822e20d79
Imam Muhajir <imammuhajir992@gmail.com>
2022-12-17 07:24:21 +0000

    add category_retention

---
+++ models/process/category_count.sql
@@ -0,0 +1,5 @@
+{{ config(materialized='view') }}
+
+select * , 
+row_number() OVER(partition by  customer_unique_id, product_category_name order by order_purchase_timestamp ) as purchase_to 
+from {{ref('category_merge')}}
